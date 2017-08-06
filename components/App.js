d67c0147e2560f75ed62b7701549099822218598
Ridge <rto@MBAir.local>
2017-08-05 20:33:12 -0700

    add swipe components and get up and running

--- components/App.js
+++ components/App.js
@@ -5,21 +5,13 @@ import {
   Text,
   View
 } from 'react-native';
+import CardContainer from './CardContainer.js';
 
 export default class NewSwipeRN extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <CardContainer/>
       </View>
     );
   }
@@ -31,16 +23,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
---
+++ components/CardContainer.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { View, FlatList, Button, Dimensions } from 'react-native';
+import Card from './Card.js';
+
+const DIMENSIONS = Dimensions.get('window');
+
+export default class CardContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      articles: [],
+    };
+  }
+
+  handleAdd = async () => {
+    try {
+      const res = await fetch('https://newsapi.org/v1/articles?source=techcrunch&apiKey=608bdc30ab01443daca0c1768e10127d');
+      const result = await res.json();
+      console.log(result.articles)
+      this.setState({
+        articles: [result.articles, ...this.state.articles],
+      });
+      console.log(this.state.articles)
+    } catch (err) {
+      alert(JSON.stringify(err));
+    }
+  };
+
+  handleRemove = (index) => {
+    const start = this.state.articles.slice(0, index);
+    const end = this.state.articles.slice(index + 1);
+    this.setState({
+      articles: start.concat(end),
+    });
+  };
+
+  componentWillMount() {
+    this.handleAdd();
+  }
+
+  render() {
+    return (
+      <View>
+        <FlatList
+          style={{ marginTop: 20, height: DIMENSIONS.height, width: DIMENSIONS.width }}
+          contentContainerStyle={{flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center', borderColor: 'red', borderWidth: 1}}
+          data={this.state.articles[0]}
+          renderItem={({ item, index }) => (
+            <Card
+              {...item}
+              index={index}
+              onSwipe={this.handleRemove}
+            />
+          )}
+          keyExtractor={(item) => item.url}
+          scrollEnabled={false}
+        />
+      </View>
+    );
+  }
+}
